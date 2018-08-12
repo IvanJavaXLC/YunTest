f4667bcbacfac276a8271048d93b7c811789c390
xlc <ivan_java_2016@163.com>
2018-08-12 18:34:56 +0800

    收获更多的东西

--- duorou/src/main/java/getter.java
+++ duorou/src/main/java/getter.java
@@ -1,5 +1,7 @@
 public class getter {
     public static void main(String[] args) {
         System.out.println("获得果实一斤");
+        System.out.println("收获种子1g");
+        System.out.println("收获果皮10g");
     }
 }
