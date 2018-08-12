fe65abbdec8ec21fe1841e56a1993e8e5e17f508
xlc <ivan_java_2016@163.com>
2018-08-12 19:47:08 +0800

    营养液

--- duorou/src/main/java/plant.java
+++ duorou/src/main/java/plant.java
@@ -1,7 +1,7 @@
 public class plant {
     public static void main(String[] args) {
         System.out.println("种下一颗种子");
-        System.out.println("我来浇浇水");
+        System.out.println("我来加营养液");
         System.out.println("覆盖土壤");
     }
 }
